e35bbf26cd4515ea687bbaa1bc14e307aeb4e041
Chinedu Nwodo <chinedunwodojr@gmail.com>
2026-08-29 23:45:33 +0100

    Add member 'AssetStore' to Engine class

--- src/core/engine.hpp
+++ src/core/engine.hpp
@@ -6,12 +6,21 @@
 #include "./window.hpp"
 #include "./time.hpp"
 
+struct mesh_shape;
+struct material_data;
+
+struct AssetStore {
+    std::unordered_map<std::string, mesh_shape*> meshes;
+    std::unordered_map<std::string, material_data*> materials;
+};
+
 class Engine {
 public:
     World::Registry WorldRegistry;
     Window::Container MainWindow;
     Script::Scheduler ScriptSchuduler;
     Time Timer;
+    AssetStore Assets;
 
     Engine();
     void Update();

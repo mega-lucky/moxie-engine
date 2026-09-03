0ade510f014161363745c6d4ece8b366f15e4ec2
Chinedu Nwodo <chinedunwodojr@gmail.com>
2026-09-03 02:22:49 +0100

    Edit cmake config

--- cmake/FetchContent.cmake
+++ cmake/FetchContent.cmake
@@ -1,7 +1,5 @@
 include(FetchContent)
 
-set(FETCHCONTENT_BASE_DIR "${CMAKE_SOURCE_DIR}/cache/_deps" CACHE PATH "Custom FetchContent directory" FORCE)
-
 option(LUAU_BUILD_CLI "Build CLI" OFF)
 option(LUAU_BUILD_TESTS "Build tests" OFF)
 option(LUAU_BUILD_WEB "Build Web module" OFF)
@@ -18,36 +16,42 @@ FetchContent_Declare(
     GIT_REPOSITORY https://gitlab.freedesktop.org/freetype/freetype
     GIT_TAG VER-2-14-3
     FIND_PACKAGE_ARGS
+    SOURCE_DIR "${CMAKE_SOURCE_DIR}/cache/external/freetype-src"
 )
 FetchContent_Declare(
     glfw3
     GIT_REPOSITORY https://github.com/glfw/glfw
     GIT_TAG 3.4
     FIND_PACKAGE_ARGS
+    SOURCE_DIR "${CMAKE_SOURCE_DIR}/cache/external/glfw3-src"
 )
 FetchContent_Declare(
     googlebenchmark
     GIT_REPOSITORY https://github.com/google/benchmark.git
     GIT_TAG v1.9.5
     FIND_PACKAGE_ARGS
+    SOURCE_DIR "${CMAKE_SOURCE_DIR}/cache/external/googlebenchmark-src"
 )
 FetchContent_Declare(
     cglm
     GIT_REPOSITORY https://github.com/recp/cglm.git
     GIT_TAG v0.9.6
     FIND_PACKAGE_ARGS
+    SOURCE_DIR "${CMAKE_SOURCE_DIR}/cache/external/cglm-src"
 )
 FetchContent_Declare(
     glm
     GIT_REPOSITORY https://github.com/g-truc/glm.git
     GIT_TAG 1.0.3
     FIND_PACKAGE_ARGS
+    SOURCE_DIR "${CMAKE_SOURCE_DIR}/cache/external/glm-src"
 )
 FetchContent_Declare(
     luau
     GIT_REPOSITORY https://github.com/luau-lang/luau.git
     GIT_TAG 0.735
     FIND_PACKAGE_ARGS
+    SOURCE_DIR "${CMAKE_SOURCE_DIR}/cache/external/luau-src"
 )
 
 FetchContent_MakeAvailable(freetype)

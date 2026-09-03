4798a1c0729d15ab57af6dd4e4859a5353709f51
Chinedu Nwodo <chinedunwodojr@gmail.com>
2026-09-03 15:38:47 +0100

    Query cameras in render system

--- src/systems/render/render_system.cpp
+++ src/systems/render/render_system.cpp
@@ -8,7 +8,8 @@ RenderSystem::RenderSystem(Engine &e) :
     world(e.WorldRegistry),
     MeshRender(world.GetComponentID("MeshRender")),
     MeshShape(world.GetComponentID("MeshShape")),
-    Transform(world.GetComponentID("Transform"))
+    Transform(world.GetComponentID("Transform")),
+    Camera(world.GetComponentID("Camera"))
 {
     glClearColor(1.0f,1.0f,0.0f,1.0f);
     glEnable(GL_DEPTH_TEST);
@@ -19,6 +20,36 @@ void RenderSystem::Update(double dt) {
     (void)dt;
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     draw_calls.clear();
+    cam_matrices.clear();
+
+    World::Query(world, {Camera, Transform}).Each([this](Entity camera){
+        transform &t = world.GetComponent<transform>(camera, Transform);
+        camera_data &c = world.GetComponent<camera_data>(camera, Camera);
+
+        mat4 model = {0};
+
+        glm_quat_mat4(t.rotation, model);
+
+        model[0][0] *= t.scale[0];
+        model[1][1] *= t.scale[1];
+        model[2][2] *= t.scale[2];
+
+        model[3][0] = t.position[0];
+        model[3][1] = t.position[1];
+        model[3][2] = t.position[2];
+
+        view_proj vp = { .view={0}, .proj={0} };
+
+        glm_mat4_inv(model, vp.view);
+
+        if (c.projection == projection_Perspective) {
+            glm_perspective(c.frustrum.fieldofview, c.frustrum.aspect, c.near, c.far, vp.proj);
+        } else if (c.projection == projection_Orthographic) {
+            glm_ortho(c.box.left, c.box.right, c.box.bottom, c.box.top, c.near, c.far, vp.proj);
+        }
+
+        cam_matrices.push_back(std::move(vp));
+    });
 
     World::Query(world, {MeshRender, MeshShape, Transform}).Each([this](Entity entity){
         transform &t = world.GetComponent<transform>(entity, Transform);
@@ -34,9 +65,9 @@ void RenderSystem::Update(double dt) {
                 .material = r.material,
             }
         };
-        
+
         glm_quat_mat4(t.rotation, call.mesh.model);
-        
+
         call.mesh.model[0][0] *= t.scale[0];
         call.mesh.model[1][1] *= t.scale[1];
         call.mesh.model[2][2] *= t.scale[2];
--- src/systems/render/render_system.hpp
+++ src/systems/render/render_system.hpp
@@ -11,11 +11,13 @@
 class RenderSystem : public World::IWorldSystem {
 private:
     std::vector<draw_call> draw_calls;
+    std::vector<view_proj> cam_matrices;
     World::Registry &world;
 
     ComponentID MeshRender;
     ComponentID MeshShape;
     ComponentID Transform;
+    ComponentID Camera;
 public:
     RenderSystem(Engine &e);
     void Update(double dt) override;

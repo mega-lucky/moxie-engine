0fc082b8d47b4709c9745a451fbb72ce2530287b
Chinedu Nwodo <chinedunwodojr@gmail.com>
2026-09-19 11:30:07 +0100

    Update SAT overlap test

--- src/systems/physics/collision.c
+++ src/systems/physics/collision.c
@@ -1,84 +1,59 @@
 #include "./types.h"
+#include <string.h>
 
-void obb_axis_min_max(vec3 *verts, vec3 axis, float *min, float *max) {
-    *min = FLT_MAX; *max = -FLT_MAX;
+static inline void gen_obb_vertices(obb_collider *obb, vec3 p, vec3 *out) {
+    mat3 r; glm_quat_mat3(obb->r, r);
+
+    vec3 c, s;
+    glm_vec3_add(obb->min, obb->max, c);
+    glm_vec3_divs(c, 2.0f, c);
+    glm_mat3_mulv(r, c, c);
+    glm_vec3_add(c, p, c);
+
+    glm_vec3_sub(obb->max, obb->min, s);
+    glm_vec3_divs(s, 2.0f, s);
+
+    vec3 result[8] = {
+        { s[0],  s[1],  s[2]}, {-s[0],  s[1],  s[2]},
+        { s[0], -s[1],  s[2]}, { s[0],  s[1], -s[2]},
+        { s[0], -s[1], -s[2]}, {-s[0],  s[1], -s[2]},
+        {-s[0], -s[1],  s[2]}, {-s[0], -s[1], -s[2]},
+    };
+
+    memcpy(out, result, sizeof(vec3) * 8);
 
     for (int i = 0; i < 8; i ++) {
-        float projection = glm_vec3_dot(axis, verts[i]);
+        glm_mat3_mulv(r, out[i], out[i]);
+        glm_vec3_add(out[i], c, out[i]);
+    }
+}
+
+static inline void gen_obb_normals(obb_collider *obb, vec3 *out) {
+    glm_quat_rotatev(obb->r, GLM_XUP, out[0]);
+    glm_quat_rotatev(obb->r, GLM_YUP, out[1]);
+    glm_quat_rotatev(obb->r, GLM_ZUP, out[2]);
+}
+
+static inline void bb_cross_axes(vec3 *a_normals, vec3 *b_normals, vec3 *out) {
+    for (int i = 0; i < 3; i ++) {
+        for (int j = 0; j < 3; j ++) {
+            glm_vec3_cross(a_normals[i], b_normals[j], out[i * 3 + j]);
+        }
+    }
+}
+
+static inline void sat_min_max(shape *a, vec3 axis, float *min, float *max) {
+    *min = FLT_MAX; *max = -FLT_MAX;
+
+    for (size_t i = 0; i < a->n_verts; i ++) {
+        float projection = glm_vec3_dot(axis, a->verts[i]);
         *min = fminf(*min, projection);
         *max = fmaxf(*max, projection);
     }
 }
 
-bool obb_vs_obb(obb_collider *a, obb_collider *b, vec3 p0, vec3 p1) {
-    mat3 r0, r1;
-    glm_quat_mat3(a->r, r0);
-    glm_quat_mat3(b->r, r1);
-
-    vec3 c0, s0;
-    glm_vec3_add(a->min, a->max, c0);
-    glm_vec3_divs(c0, 2.0f, c0);
-    glm_mat3_mulv(r0, c0, c0);
-    glm_vec3_add(c0, p0, c0);
-
-    glm_vec3_sub(a->max, a->min, s0);
-    glm_vec3_divs(s0, 2.0f, s0);
-    
-    vec3 c1, s1;
-    glm_vec3_add(b->min, b->max, c1);
-    glm_vec3_divs(c1, 2.0f, c1);
-    glm_mat3_mulv(r1, c1, c1);
-    glm_vec3_add(c1, p1, c1);
-    
-    glm_vec3_sub(b->max, b->min, s1);
-    glm_vec3_divs(s1, 2.0f, s1);
-
-    vec3 axes[15];
-    glm_quat_rotatev(a->r, GLM_XUP, axes[0]);
-    glm_quat_rotatev(a->r, GLM_YUP, axes[1]);
-    glm_quat_rotatev(a->r, GLM_ZUP, axes[2]);
-
-    glm_quat_rotatev(b->r, GLM_XUP, axes[3]);
-    glm_quat_rotatev(b->r, GLM_YUP, axes[4]);
-    glm_quat_rotatev(b->r, GLM_ZUP, axes[5]);
-
-    for (int i = 0; i < 3; i ++) {
-        for (int j = 0; j < 3; j ++) {
-            glm_vec3_cross(axes[i], axes[j + 3], axes[6 + j * 3 + i]);
-        }
-    }
-
-    vec3 verts0[8] = {
-        { s0[0],  s0[1],  s0[2]},
-        {-s0[0],  s0[1],  s0[2]},
-        { s0[0], -s0[1],  s0[2]},
-        { s0[0],  s0[1], -s0[2]},
-        { s0[0], -s0[1], -s0[2]},
-        {-s0[0],  s0[1], -s0[2]},
-        {-s0[0], -s0[1],  s0[2]},
-        {-s0[0], -s0[1], -s0[2]},
-    };
-
-    vec3 verts1[8] = {
-        { s1[0],  s1[1],  s1[2]},
-        {-s1[0],  s1[1],  s1[2]},
-        { s1[0], -s1[1],  s1[2]},
-        { s1[0],  s1[1], -s1[2]},
-        { s1[0], -s1[1], -s1[2]},
-        {-s1[0],  s1[1], -s1[2]},
-        {-s1[0], -s1[1],  s1[2]},
-        {-s1[0], -s1[1], -s1[2]},
-    };
-
-    for (int i = 0; i < 8; i ++) {
-        glm_mat3_mulv(r0, verts0[i], verts0[i]);
-        glm_vec3_add(verts0[i], c0, verts0[i]);
-
-        glm_mat3_mulv(r1, verts1[i], verts1[i]);
-        glm_vec3_add(verts1[i], c1, verts1[i]);
-    }
-
-    for (int i = 0; i < 15; i ++) {
+static inline bool seperating_axis(shape *a, shape *b, vec3 *axes, size_t n_axes) {
+    for (size_t i = 0; i < n_axes; i ++) {
         if (glm_vec3_norm2(axes[i]) < GLM_FLT_EPSILON) {
             continue;
         }
@@ -86,13 +61,29 @@ bool obb_vs_obb(obb_collider *a, obb_collider *b, vec3 p0, vec3 p1) {
         float *axis = axes[i];
         float min0, max0, min1, max1;
 
-        obb_axis_min_max(verts0, axis, &min0, &max0);
-        obb_axis_min_max(verts1, axis, &min1, &max1);
+        sat_min_max(a, axis, &min0, &max0);
+        sat_min_max(b, axis, &min1, &max1);
 
         if (max0 < min1 || max1 < min0) {
-            return false;
+            return true;
         }
     }
 
-    return true;
+    return false;
+}
+
+bool obb_vs_obb(obb_collider *a, obb_collider *b, vec3 p0, vec3 p1) {
+    vec3 axes[15];
+
+    gen_obb_normals(a, axes);
+    gen_obb_normals(b, axes + 3);
+    bb_cross_axes(axes, axes + 3, axes + 6);
+
+    vec3 verts0[8]; gen_obb_vertices(a, p0, verts0);
+    vec3 verts1[8]; gen_obb_vertices(b, p1, verts1);
+
+    shape s0 = {8, verts0};
+    shape s1 = {8, verts1};
+
+    return !seperating_axis(&s0, &s1, axes, 15);
 }
--- src/systems/physics/types.h
+++ src/systems/physics/types.h
@@ -8,23 +8,24 @@
 #include <cglm/cglm.h>
 
 typedef struct aabb_collider {
-    bool active;
     vec3 min, max;
 } aabb_collider;
 
 typedef struct obb_collider {
-    bool active;
     vec3 min, max; versor r;
 } obb_collider;
 
 typedef struct custom_collider {
-    bool active;
     vec3 *vertices; size_t nverts;
 } custom_collider;
 
 typedef struct mesh_shape_collider {
-    bool active;
     size_t pos_attrib;
 } mesh_shape_collider;
+
+typedef struct shape {
+    size_t n_verts;
+    vec3 *verts;
+} shape;
 
 #endif
